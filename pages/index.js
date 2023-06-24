2b2e2c82cc34f6043dbe746f5124b9bc4dea97aa
Bernardo Stogmuller Vieira <bernardostogmuller94@gmail.com>
2023-06-24 12:55:42 +0000

    add lint scripts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Manézinha eu te amo demais. ❤️</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Manézinha eu te amo demais. ❤️</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
